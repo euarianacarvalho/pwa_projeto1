726b01d9a144f6e6d1589dacc13a1dfebe62b482
Reny <renylmg@gmail.com>
2022-02-18 20:22:07 -0300

    criação, inserção e impressão dos dados do Banco de Dados

---
+++ twa_aula_1/Models/Atleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twa_aula_1.Models
+{
+    public class Atleta
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+    }
+}
--- twa_aula_1/Program.cs
+++ twa_aula_1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using twa_aula_1.Models;
 
 namespace twa_aula_1
 {
@@ -13,18 +16,81 @@ namespace twa_aula_1
 
             conexao.Open();
 
-            var sql = "INSERT INTO atleta (id, nome, altura, peso) VALUES (@id, @nome, @altura, @peso)";
-            var cmd = new MySqlCommand(sql, conexao);
+            Inserir (conexao);
 
-            cmd.Parameters.AddWithValue("@id", "1");
-            cmd.Parameters.AddWithValue("@nome", "Ana");
-            cmd.Parameters.AddWithValue("@altura", 1.7);
-            cmd.Parameters.AddWithValue("@peso", 70);
-
-            cmd.ExecuteNonQuery();
+            Selecionar(conexao);
 
             conexao.Close();
 
+        }
+
+        private static void Selecionar(MySqlConnection conexao)
+        {
+            var sql = "select * from atleta order by nome";
+
+            var cmd = new MySqlCommand(sql, conexao);
+
+            var da = new MySqlDataAdapter(cmd);
+
+            var regs = new DataTable();
+
+            da.Fill(regs);
+
+            var lista = new List<Atleta>();
+
+            foreach (DataRow reg in regs.Rows)
+            {
+                lista.Add(
+                    new Atleta
+                    {
+                        Id = reg["id"].ToString(),
+                        Nome = reg["nome"].ToString(),
+                        Altura = Convert.ToDouble(reg["altura"]),
+                        Peso = Convert.ToDouble(reg["peso"])
+                    }
+                    );
+            }
+            Imprimir(lista);
+        }
+
+        private static void Imprimir(List<Atleta> lista)
+        {
+            foreach(var obj in lista)
+            {
+                Console.WriteLine($"{obj.Nome}, altura: {obj.Altura: N2}, peso: {obj.Peso: N2}");
+            }
+        }
+
+        private static void Inserir (MySqlConnection conexao)
+        {
+            var sql = "INSERT INTO atleta (id, nome, altura, peso) VALUES (@id, @nome, @altura, @peso)";
+
+            var obj = LeAtleta();
+
+            var cmd = new MySqlCommand(sql, conexao);
+
+            cmd.Parameters.AddWithValue("@id", obj.Id);
+            cmd.Parameters.AddWithValue("@nome", obj.Nome);
+            cmd.Parameters.AddWithValue("@altura", obj.Altura);
+            cmd.Parameters.AddWithValue("@peso", obj.Peso);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        private static Atleta LeAtleta()
+        {
+            var obj = new Atleta();
+
+            obj.Id = Guid.NewGuid().ToString();
+            Console.WriteLine("Atleta: ");
+            Console.WriteLine(" - Nome: ");
+            obj.Nome = Console.ReadLine();
+            Console.WriteLine(" - Altura: ");
+            obj.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" - Peso: ");
+            obj.Peso = Convert.ToDouble(Console.ReadLine());
+
+            return obj;
         }
     }
 }
