cf3922d3cff3ada2e7f53e1d3bd194e4ce4d9e6a
Reny <renylmg@gmail.com>
2022-02-17 21:23:03 -0300

    Add project files.

---
+++ twa_aula_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace twa_aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StringConexao = "server=localhost:3306;uid=root;passord=;database=Teste";
+            var conexao = new MySqlConnection(StringConexao);
+
+            conexao.Open();
+
+            var sql = "INSERT INTO Atleta (id, nome, altura, peso) VALUES (@id, @nome, @altura, @peso)";
+            var cmd = new MySqlCommand(sql, conexao);
+
+
+
+            conexao.Close();
+
+        }
+    }
+}
